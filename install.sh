23a5aab6a309da8ade0c0c7412ddec3c8718494e
root <kaichiaboy@gmail.com>
2019-12-26 17:17:14 +0000

    update install script

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ cd $(dirname $0)
 ln .vimrc ~/.vimrc
 mkdir -p ~/.vim/vimrcs
 ln vimrcs/* ~/.vim/vimrcs/
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
